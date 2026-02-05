5dd54080eee4f6ffa2401ed20b0130405e4e9a33
HeyHydrone <lpevans001@gmail.com>
2026-02-04 21:33:27 -0500

    Update CalendarManifest.php

--- src/Entities/CalendarManifest.php
+++ src/Entities/CalendarManifest.php
@@ -19,20 +19,20 @@ class CalendarManifest
      * Create a new CalendarManifest object.
      */
     public static function create(LeekDuckEventType $eventType): self
-    {
-        return new self(
-            eventType: $eventType,
-            calendar: Calendar::create()
-                ->name(
-                    name: 'GO Calendar - ' . $eventType->title . ($eventType->title == CalendarService::EVERYTHING_CALENDAR_NAME ? '' : ' (' . acronymForEventType($eventType) . ')')
-                )
-                ->description(
-                    description: 'All Pokémon GO ' . ($eventType->title == CalendarService::EVERYTHING_CALENDAR_NAME ? '' : "{$eventType->title} ") . 'events, in your local time, auto-updated and sourced from Leek Duck.'
-                )
-                ->refreshInterval(
-                    minutes: 1440 // 1 day
-                )
-                ->timezone('America/New_York')
-        );
-    }
+{
+    return new self(
+        eventType: $eventType,
+        calendar: Calendar::create()
+            ->name(
+                'GO Calendar - ' . $eventType->title .
+                ($eventType->title == CalendarService::EVERYTHING_CALENDAR_NAME ? '' : ' (' . acronymForEventType($eventType) . ')')
+            )
+            ->description(
+                'All Pokémon GO ' . ($eventType->title == CalendarService::EVERYTHING_CALENDAR_NAME ? '' : "{$eventType->title} ") .
+                'events, in your local time, auto-updated and sourced from Leek Duck.'
+            )
+            ->refreshInterval(1440)
+    );
+}
+
 }
